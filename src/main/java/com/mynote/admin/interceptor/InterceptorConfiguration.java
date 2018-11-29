d20e77fd0018334ef9e04ea7d82f1c734cd7b728
panyi <yuyi.py@alibaba-inc.com>
2018-11-29 21:05:09 +0800

    login page

--- src/main/java/com/mynote/admin/interceptor/InterceptorConfiguration.java
+++ src/main/java/com/mynote/admin/interceptor/InterceptorConfiguration.java
@@ -8,11 +8,11 @@ public class InterceptorConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册拦截器
-        InterceptorRegistration ir = registry.addInterceptor(new LoginInterceptor());
+        //InterceptorRegistration ir = registry.addInterceptor(new LoginInterceptor());
         // 配置拦截的路径
-        ir.addPathPatterns("/**");
+        //ir.addPathPatterns("/**");
         // 配置不拦截的路径
-        ir.excludePathPatterns("/login.html");
+        //ir.excludePathPatterns("/login.html");
 
         // 还可以在这里注册其它的拦截器
         //registry.addInterceptor(new OtherInterceptor()).addPathPatterns("/**");
