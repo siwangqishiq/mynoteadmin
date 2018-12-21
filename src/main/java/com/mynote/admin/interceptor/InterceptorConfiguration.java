9787412949885d4832ee06e8e297e2f25616c79d
panyi <525647740@qq.com>
2018-12-21 23:56:08 +0800

    添加单元测试

--- src/main/java/com/mynote/admin/interceptor/InterceptorConfiguration.java
+++ src/main/java/com/mynote/admin/interceptor/InterceptorConfiguration.java
@@ -8,18 +8,18 @@ public class InterceptorConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册拦截器
-        InterceptorRegistration ir = registry.addInterceptor(new LoginInterceptor());
-        // 配置拦截的路径
-        ir.addPathPatterns("/**");
-//        ir.addPathPatterns("/api/**");
-//        ir.addPathPatterns("/page/**");
-
-        // 配置不拦截的路径
-        ir.excludePathPatterns("/login.html");
-        ir.excludePathPatterns("/api/dologin");
-        ir.excludePathPatterns("/lib/**");
-        ir.excludePathPatterns("/img/**");
-        ir.excludePathPatterns("/css/**");
+//        InterceptorRegistration ir = registry.addInterceptor(new LoginInterceptor());
+//        // 配置拦截的路径
+//        ir.addPathPatterns("/**");
+////        ir.addPathPatterns("/api/**");
+////        ir.addPathPatterns("/page/**");
+//
+//        // 配置不拦截的路径
+//        ir.excludePathPatterns("/login.html");
+//        ir.excludePathPatterns("/api/dologin");
+//        ir.excludePathPatterns("/lib/**");
+//        ir.excludePathPatterns("/img/**");
+//        ir.excludePathPatterns("/css/**");
 
         // 还可以在这里注册其它的拦截器
         //registry.addInterceptor(new OtherInterceptor()).addPathPatterns("/**");
